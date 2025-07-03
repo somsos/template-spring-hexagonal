8a3b1945fdac140035f51a67c7131d3472187a9d
mario <somsos112@gmail.com>
2025-07-02 19:17:20 -0600

    moving cros origin to accept only localhost ti any ip

--- adapter/src/main/java/daj/adapter/common/authConfig/AuthConfig.java
+++ adapter/src/main/java/daj/adapter/common/authConfig/AuthConfig.java
@@ -143,7 +143,7 @@ public class AuthConfig {
 
     private CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
